0d0e8efec454b0a2cc1a12958f2208ccdcacd1ae
root <root@localhost.localdomain>
2019-02-22 02:27:31 +0530

    added .next files

---
+++ .next/static/webpack/static/development/pages/home.js.f9110929bb33d0c2c67a.hot-update.js
@@ -0,0 +1,79 @@
+webpackHotUpdate("static/development/pages/home.js",{
+
+/***/ "./pages/home/dataConstants.js":
+/*!*************************************!*\
+  !*** ./pages/home/dataConstants.js ***!
+  \*************************************/
+/*! exports provided: historyList, projectsList, articlesList, skillsetList */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "historyList", function() { return historyList; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "projectsList", function() { return projectsList; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "articlesList", function() { return articlesList; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "skillsetList", function() { return skillsetList; });
+var projectsList = [{
+  title: 'Void Messenger - Secure code chatting application',
+  hightLight: 'Void Messenger',
+  hexCode: '#EA4C89',
+  link: ''
+}, {
+  title: 'KeyLogger - Custom windows keylogger',
+  hightLight: 'KeyLogger',
+  hexCode: '#EA4C89',
+  link: ''
+}, {
+  title: 'CommunicApp - Messaging andoird application',
+  hightLight: 'CommunicApp',
+  hexCode: '#EA4C89',
+  link: ''
+}, {
+  title: 'Legal Recourse',
+  hightLight: 'Legal Recourse',
+  hexCode: '#EA4C89',
+  link: ''
+}, {
+  title: 'Mywaze - GeoSocial networking android application',
+  hightLight: 'Mywaze',
+  hexCode: '#EA4C89',
+  link: ''
+}];
+var historyList = [{
+  title: '2019 / Present - Software Developer - Cuemath - Bengaluru, India',
+  hightLight: 'Cuemath',
+  hexCode: '#DF5DFF',
+  link: 'https://www.cuemath.com'
+}, {
+  title: '2017 - Software Developer Intern - Cuemath - Bengaluru, India',
+  hightLight: 'Cuemath',
+  hexCode: '#DF5DFF',
+  link: 'https://www.cuemath.com'
+}];
+var articlesList = [{
+  title: 'How to setup a virtual private server from scratch 27 Aug 2018',
+  hightLight: '27 Aug 2018',
+  hexCode: '#EA4C89',
+  link: ''
+}, {
+  title: 'Basic file operations in Python 28 Nov 2017',
+  hightLight: '28 Nov 2017',
+  hexCode: '#EA4C89',
+  link: ''
+}];
+var skillsetList = [{
+  title: 'Languages',
+  value: ['Python', 'Javascript', 'Java', 'Node.js', 'HTML5', 'CSS3', 'C++', 'SQL']
+}, {
+  title: 'Tools/OS',
+  value: ['Vim', 'Bash', 'Docker (Currently having fun with)', 'Ubuntu(Linux)', 'Kali', 'Windows']
+}, {
+  title: 'Frameworks',
+  value: ['Django', 'NEXT JS', 'Android', 'Flask', 'React.js']
+}];
+
+
+/***/ })
+
+})
+//# sourceMappingURL=home.js.f9110929bb33d0c2c67a.hot-update.js.map
