17ebcef18c58e3e5be917c4d60dc4cc32f41a1b8
root <root@localhost.localdomain>
2019-03-06 01:05:02 +0530

    have to revert gitignore for build to work with all the files on heroku

---
+++ .next/static/webpack/static/development/pages/home.js.7c4c4c191044424bded8.hot-update.js
@@ -0,0 +1,39 @@
+webpackHotUpdate("static/development/pages/home.js",{
+
+/***/ "./components/wrappers.js":
+/*!********************************!*\
+  !*** ./components/wrappers.js ***!
+  \********************************/
+/*! exports provided: Container, CompressContainer, SectionContainer, HalfContainer */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Container", function() { return Container; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "CompressContainer", function() { return CompressContainer; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "SectionContainer", function() { return SectionContainer; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "HalfContainer", function() { return HalfContainer; });
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+
+var Container = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "wrappers__Container",
+  componentId: "sc-1mlzmjp-0"
+})(["height:inherit;margin:0 auto;padding:16px;"]);
+var CompressContainer = Object(styled_components__WEBPACK_IMPORTED_MODULE_0__["default"])(Container).withConfig({
+  displayName: "wrappers__CompressContainer",
+  componentId: "sc-1mlzmjp-1"
+})(["@media (min-width:768px){width:80%;margin:16px;}"]);
+var HalfContainer = Object(styled_components__WEBPACK_IMPORTED_MODULE_0__["default"])(Container).withConfig({
+  displayName: "wrappers__HalfContainer",
+  componentId: "sc-1mlzmjp-2"
+})(["max-width:1028px;margin:0px 32px;"]);
+var SectionContainer = styled_components__WEBPACK_IMPORTED_MODULE_0__["default"].div.withConfig({
+  displayName: "wrappers__SectionContainer",
+  componentId: "sc-1mlzmjp-3"
+})(["margin-bottom:2em !important;"]);
+
+
+/***/ })
+
+})
+//# sourceMappingURL=home.js.7c4c4c191044424bded8.hot-update.js.map
