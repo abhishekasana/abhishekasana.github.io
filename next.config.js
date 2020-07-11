eb8dc50ebf9ede00ee0bccdb1a9edff09c99a03e
Abhishek Kasana <abhishek.kasana@cuemath.com>
2020-07-11 19:59:44 +0530

    updated collectjs script for github js assests

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  // next export -> assest path update
+  assetPrefix: prod ? '/js/' : '',
   // next export -> /out/home/index.js -> /out/index.js
   exportPathMap: async function (
     defaultPathMap,
