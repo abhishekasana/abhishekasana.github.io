9f69ddb309eb60c846584402f968970ff99ca7ff
Kasana <abhishek.kasana@cuemath.com>
2022-08-17 12:57:32 +0530

    og image banner test - build 3

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
       '/about-me': { page: '/about-me' },
       '/blog/birthday-attack': { page: '/blog/birthday-attack' },
       '/blog/github-pages-nextjs': { page: '/blog/github-pages-nextjs'},
-      '/social-banner-test-7': { page: '/social-banner-test-7'},
+      '/social-banner-test-8': { page: '/social-banner-test-7'},
       '/social-banner-test-4': { page: '/social-banner-test-4'},
       '/social-banner-test-5': { page: '/social-banner-test-5'},
       '/social-banner-test-6': { page: '/social-banner-test-6'},
