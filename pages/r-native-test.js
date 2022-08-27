07c3643523b58474097c77cddc1773795dea91ee
Kasana <abhishek.kasana@cuemath.com>
2022-08-27 17:41:09 +0530

    webview test build 2

--- pages/r-native-test.js
+++ pages/r-native-test.js
@@ -4,6 +4,7 @@ import Layout from 'components/Layout';
 
 function RNativeTest() {
     return <Layout>
+        <p>Test - {typeof window !== "undefined" && typeof window}</p>
         {typeof window !== "undefined" && window.ReactNativeWebView}
     </Layout>
 }
