01e70fa5b25131b6fce6a4322679105a1a9a41bb
Kasana <abhishek.kasana@cuemath.com>
2022-09-05 22:46:14 +0530

    banner test - 2

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
       '/blog/birthday-attack': { page: '/blog/birthday-attack' },
       '/blog/github-pages-nextjs': { page: '/blog/github-pages-nextjs'},
       '/blog/text-search-in-postgresql': { page: '/blog/text-search-in-postgresql'},
-      '/test-banner': { page: '/test-banner'},
+      '/test-banner-1': { page: '/test-banner'},
     }
   },
 };
