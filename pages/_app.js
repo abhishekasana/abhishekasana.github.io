e0f0968478f642e3acf31e5e08e3f412ee191b26
Kasana <abhishek.kasana@cuemath.com>
2024-01-01 12:15:23 +0530

    updated the meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,12 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                <meta charSet="UTF-8" />
+                <title>Abhishek Kasana | Developer | Personal Homepage</title>
+                <meta name="description" key="description" content="Discover the world of software development with Abhishek Kasana, a passionate developer and tinkerer. Explore insightful blogs on coding, innovation, and the wonders of tech." />
+                <meta name="keywords" key="keywords" content="Python, Database, Cryptography, React, Node.js, Docker, Software Engineering, Tech Innovation, Programming, Abhishek Kasana, Cuemath, Software Developer, Tech Blogger, Bangalore Tech Scene, India technology, PEC" />
+                <link rel="shortcut icon" href="/static/images/logo/fav_icon.png" />
+
                 <link rel="icon" href="/static/images/logo/fav_icon.png" type="image/png" sizes="16x16" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
             </Head>
