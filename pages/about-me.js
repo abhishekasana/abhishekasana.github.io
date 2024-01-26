1e4b1c7b6e7b96b26d7ff2d143cd7c087334c218
Kasana <abhishek.kasana@cuemath.com>
2024-01-26 17:40:42 +0530

    removed the projects list from about me section

--- pages/about-me.js
+++ pages/about-me.js
@@ -6,18 +6,16 @@ import SkillSet from 'components/home/skillset';
 import WhoAmI from 'components/home/whoami';
 import ContactMe from 'components/home/contactme';
 import { BaseContainer } from 'components/wrappers';
-import { projectsList, historyList, articlesList } from 'components/home/dataConstants';
+import { historyList } from 'components/home/dataConstants';
 
 function AboutMe() {
   return <Layout>
         <BaseContainer>
             <WhoAmI />
             <SectionList listHeader="history" taskList={historyList} collapsed={false} />
-            <SectionList listHeader="ls projects/" taskList={projectsList} collapsed />
             <SkillSet/>
             <ContactMe />
         </BaseContainer>
-        {/* <Footer /> */}
     </Layout>
 }
 export default AboutMe;
