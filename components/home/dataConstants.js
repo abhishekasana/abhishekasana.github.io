8169c336f84e98ef900a0c6c15dad08a26b4a9cd
abhishekasana <abhishekasana@gmail.com>
2025-11-22 11:55:10 +0530

    Update work history

--- components/home/dataConstants.js
+++ components/home/dataConstants.js
@@ -1,101 +1,93 @@
 const projectsList = [
-    {
-        title: 'Void Messenger - Secure code chatting application',
-        highlight: 'Void Messenger',
-        hexCode: '#EA4C89',
-        link: ''
-    },
-    {
-        title: 'KeyLogger - Custom windows keylogger',
-        highlight: 'KeyLogger',
-        hexCode: '#EA4C89',
-        link: ''
-    },
-    {
-        title: 'Legal Recourse - Web application that suggest\'s the right legal representation',
-        highlight: 'Legal Recourse',
-        hexCode: '#EA4C89',
-        link: ''
-    },
-    {
-        title: 'Mywaze - GeoSocial networking android application',
-        highlight: 'Mywaze',
-        hexCode: '#EA4C89',
-        link: ''
-    },
+  {
+    title: 'Void Messenger - Secure code chatting application',
+    highlight: 'Void Messenger',
+    hexCode: '#EA4C89',
+    link: '',
+  },
+  {
+    title: 'KeyLogger - Custom windows keylogger',
+    highlight: 'KeyLogger',
+    hexCode: '#EA4C89',
+    link: '',
+  },
+  {
+    title:
+      "Legal Recourse - Web application that suggest's the right legal representation",
+    highlight: 'Legal Recourse',
+    hexCode: '#EA4C89',
+    link: '',
+  },
+  {
+    title: 'Mywaze - GeoSocial networking android application',
+    highlight: 'Mywaze',
+    hexCode: '#EA4C89',
+    link: '',
+  },
 ];
 const historyList = [
-    {
-        title: '2018 / Present - Lead Software Developer - Cuemath - Bengaluru, India',
-        highlight: 'Cuemath',
-        hexCode: '#DF5DFF',
-        link: 'https://www.cuemath.com'
-    },
-    {
-        title: '2017 - Software Developer Intern - Cuemath - Bengaluru, India',
-        highlight: 'Cuemath',
-        hexCode: '#DF5DFF',
-        link: 'https://www.cuemath.com',
-    }
+  {
+    title: '2025 / Present - Senior Software Engineer - Rippling - Bengaluru, India',
+    highlight: 'Rippling',
+    hexCode: '#DF5DFF',
+    link: 'http://www.rippling.com',
+  },
 
+  {
+    title: '2018 / Present - Lead Software Developer - Cuemath - Bengaluru, India',
+    highlight: 'Cuemath',
+    hexCode: '#DF5DFF',
+    link: 'https://www.cuemath.com',
+  },
+  {
+    title: '2017 - Software Developer Intern - Cuemath - Bengaluru, India',
+    highlight: 'Cuemath',
+    hexCode: '#DF5DFF',
+    link: 'https://www.cuemath.com',
+  },
 ];
 const articlesList = [
-    // {
-    //     title: 'How to setup a virtual private server from scratch 27 Aug 2018',
-    //     highlight: '27 Aug 2018',
-    //     hexCode: '#EA4C89',
-    //     link: ''
-    // },
+  // {
+  //     title: 'How to setup a virtual private server from scratch 27 Aug 2018',
+  //     highlight: '27 Aug 2018',
+  //     hexCode: '#EA4C89',
+  //     link: ''
+  // },
 ];
 
 const skillSetList = [
-    {
-        title: 'Languages',
-        value: [
-            'Python',
-            'Javascript',
-            'Java',
-            'Node.js',
-            'HTML5',
-            'CSS3',
-            'C++',
-            'SQL',
-        ],
-    },
-    {
-        title: 'Tools/OS',
-        value: [
-            'Vim',
-            'Bash',
-            'Docker (Currently having fun with)',
-            'Ubuntu(Linux)',
-            'Kali',
-            'Windows'
-        ],
-    },
-    {
-        title: 'Frameworks',
-        value: [
-            'Django',
-            'NEXT JS',
-            'Android',
-            'Flask',
-            'React.js',
-        ],
-    },
-    // {
-    //     title: 'Other',
-    //     value: [
-    //         'HTTP / REST / OAuth2 / API Development',
-    //         'Flexbox',
-    //         'Git',
-    //         'Celery',
-    //         'Basic UNIX Admin',
-    //         'OOP',
-    //         'Nginx',
-    //         'AWS (EC2, S3)',
-    //     ],
-    // },
+  {
+    title: 'Languages',
+    value: ['Python', 'Javascript', 'Java', 'Node.js', 'HTML5', 'CSS3', 'C++', 'SQL'],
+  },
+  {
+    title: 'Tools/OS',
+    value: [
+      'Vim',
+      'Bash',
+      'Docker (Currently having fun with)',
+      'Ubuntu(Linux)',
+      'Kali',
+      'Windows',
+    ],
+  },
+  {
+    title: 'Frameworks',
+    value: ['Django', 'NEXT JS', 'Android', 'Flask', 'React.js'],
+  },
+  // {
+  //     title: 'Other',
+  //     value: [
+  //         'HTTP / REST / OAuth2 / API Development',
+  //         'Flexbox',
+  //         'Git',
+  //         'Celery',
+  //         'Basic UNIX Admin',
+  //         'OOP',
+  //         'Nginx',
+  //         'AWS (EC2, S3)',
+  //     ],
+  // },
 ];
 
 const contactMe = `I'm currently looking for remote or LDN/MTL/EU based work,
@@ -105,9 +97,4 @@ const whoAmI = `I'm a massive cyberpunk nerd with an interest in open source and
  In my free time I like to hack on fun projects, play / watch football with friends,
  gulp banana smoothies and take pictures (among other things, like obsessing over my Hacker News karma).`;
 
-export {
-    historyList,
-    projectsList,
-    articlesList,
-    skillSetList,
-};
+export { historyList, projectsList, articlesList, skillSetList };
